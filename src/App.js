1cff2f15a647175134ddcbe4a2f44cf888e2cc17
Abhishek Choubey <choubeyabhishek53@gmail.com>
2023-12-24 10:58:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import PT from "prop-types";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function AEmployee(props) {
+    const { name, empId, gender, status, friendList, studentDetails, fun1, fun2, fun3, fun4 } = props;
+    const { mobileNo, emailId } = studentDetails;
+    return (
+        <>
+            <h2>Employee Details</h2>
+            <hr />
+            <h2>Hello, {name}</h2>
+            <h2>Your Emp Id : {empId }</h2>
+            <h2>Your Gender : {gender ? 'Male' : 'Female'}</h2>
+            <h2>Your Status : {status ? 'New' : 'Old'}</h2>
+            <h2>Friend List</h2>
+            <ul>
+                {
+                    friendList.map((n, i) => {
+                        return <li key={i} >{n}</li>
+                    })
+                }
+            </ul>
+            <h4>Stduent Info</h4>
+            <p>MobileNo :  {mobileNo}</p>
+            <p>EmailId :  {emailId}</p>
+            <h4>Fun1: {fun1()}</h4>
+            <h4>Fun2: {fun2()}</h4>
+            <h4>Fun3: {fun3("How are you ?")}</h4>
+            <h4>Fun4: {fun4(10, 20)}</h4>
+        </>
+    )
 }
 
-export default App;
+//PropTypes only give warning in console.
+
+AEmployee.propTypes={
+
+    name: PT.string,
+    empId:PT.number,
+    //empId:PT.any,   //to send any kind of data.
+    gender:PT.bool,
+    status:PT.bool,
+    friendList:PT.array,
+    studentDetails:PT.object,
+    fun1:PT.func,
+    fun2:PT.func,
+    fun3:PT.func,
+    fun4:PT.func,
+    isActive:PT.bool,
+
+}
+
+
+//Complex Val
+
+export class BEmployee extends Component
+{
+    render()
+    {
+        return(<>
+        
+         <h1>Hello,{this.props.name}</h1>
+         <h1>Employee ID: {this.props.empId}</h1>
+         <h1>Gender : {this.props.gender}</h1>
+         <h1>Designation:{this.props.designation} </h1>
+
+        </>)
+    }
+}
+
+//camelcase
+
+BEmployee.defaultProps={
+    designation:"Associate"
+}
+
+BEmployee.propTypes = {
+  name:PT.string.isRequired,
+  gender:PT.oneOf(['male','female']),
+  empId:PT.oneOfType([PT.number,PT.string]),
+  
+}
+
+BEmployee.defaultProps={
+    designation:"Associate"
+}
+
+
+// export function FunEmployee(props)
+// {
+   
+//         return(<>
+        
+//          <h1>Hello,{props.name}</h1>
+//          <h1>Employee ID: {props.empId}</h1>
+//          <h1>Gender : {props.gender}</h1>
+//          <h1>Designation:{props.designation} </h1>
+
+//         </>)
+    
+// }
+
+// FunEmployee.defaultProps={
+//     designation:"Associate"
+// }
+
+
+// FunEmployee.propTypes = {
+//     name:PT.string.isRequired,
+//     gender:PT.oneOf(['male','female']),
+//     empId:PT.oneOfType([PT.number,PT.string]),
+    
+//   }
+
+
+// export function AFunEmployee(props)
+// {
+   
+//         return(<>
+        
+//          <h1>Hello,</h1>
+//          <h1>Employee ID: {props.empDetails.empId}</h1>
+//          <h1>Name : {props.empDetails.name}</h1>
+//          <h1>Age : {props.empDetails.age}</h1>
+
+//         </>)
+    
+// }
+
+
+export function AFunEmployee({empDetails})
+{
+    const {empId,name,age} = empDetails;
+   
+        return(<>
+        
+         <h1>Hello,</h1>
+         <h1>Employee ID: {empId}</h1>
+         <h1>Name : {name}</h1>
+         <h1>Age : {age}</h1>
+
+        </>)
+    
+}
+
+AFunEmployee.defaultProps={
+    
+}
+
+
+AFunEmployee.propTypes = {
+   
+    empDetails:PT.shape({
+        empId:PT.number.isRequired,
+        name:PT.string.isRequired,
+        age:PT.number.isRequired
+    })
+
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Component } from "react";
+import ReactDOM from "react-dom/client";
+import {AEmployee,BEmployee,FunEmployee,AFunEmployee } from './app'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function AIndex() {
+//     let friends = ["Chandan", "Pawan", "Samir"];
+//     let details = { mobileNo: 8956235689, emailId: 'ajeet@gmail.com' };
+//     return (
+//         <AEmployee
+//             name="Ajeet Singh"
+//            // name={1234}  //Warning: Failed prop type: Invalid prop `name` of type
+//             empId={789}
+//             gender
+//             status={false}
+//             friendList={friends}
+//             studentDetails={details}
+//             fun1={() => "Hi Ajeet"}
+//             fun2={() => { return "Hi Chandan" }}
+//             fun3={(msg) => { return msg }}
+//             fun4={(a, b) => a + b}
+//             isActive
+//         />
+//     )
+// }
+// const aroot = ReactDOM.createRoot(document.getElementById("root"));
+// aroot.render(<AIndex />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//Props Complex Validation
+
+// class BIndex extends Component
+// {
+//    render()
+//    {
+//     return(<>
+
+//      {/* <BEmployee/>  Warning: Failed prop type: The prop `name` is marked as required in `BEmployee`, but its value is `undefined`. at BEmployee */}
+     
+//      {/* <BEmployee name="Arjun" gender="Male"/> */}
+
+//      <BEmployee name="Arjun" gender="male" empId={101}  />
+  
+    
+//     </>)
+//    }
+// }
+
+// const broot = ReactDOM.createRoot(document.getElementById("root"));
+// broot.render(<BIndex />);
+
+
+
+// function CIndex()
+// {
+
+//     return(<>
+    
+//      <FunEmployee name="Arjun" gender="male" empId={101} />
+
+//     </>)
+// }
+
+
+// const croot = ReactDOM.createRoot(document.getElementById("root"));
+// croot.render(<CIndex />);
+
+
+
+
+function DIndex()
+{
+    let obj={
+        empId:210,
+        name:"Rahul",
+        age:32
+    }
+    return(<>
+    
+      <AFunEmployee empDetails={obj}/>
+
+    </>)
+}
+
+
+const droot = ReactDOM.createRoot(document.getElementById("root"));
+droot.render(<DIndex />);
+
+
+
